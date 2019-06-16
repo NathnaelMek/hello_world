541d951bca6d04c78b61c355cbaa8e996d3a1a11
NathnaelMek <46406747+NathnaelMek@users.noreply.github.com>
2019-06-16 16:57:06 -0500

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--test-type")
+options.binary_location = "/usr/bin/chromium"
+
